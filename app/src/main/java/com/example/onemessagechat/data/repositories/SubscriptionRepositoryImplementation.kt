36f132e376e98e7639e1f7171809201e8440a0b1
Juan Lucas Rossi <juan.lucas.rossi@gmail.com>
2023-12-05 08:44:45 -0300

    feat: implementa os metodos de subscriptionRepository

--- app/src/main/java/com/example/onemessagechat/data/repositories/SubscriptionRepositoryImplementation.kt
+++ app/src/main/java/com/example/onemessagechat/data/repositories/SubscriptionRepositoryImplementation.kt
@@ -1,4 +1,18 @@
 package com.example.onemessagechat.data.repositories
 
-class SubscriptionRepositoryImplementation {
+import com.example.onemessagechat.domain.entities.subscribe.Subscription
+import com.example.onemessagechat.domain.repositories.SubscriptionsRepository
+import com.example.onemessagechat.infra.daos.SubscriptionDAO
+import com.example.onemessagechat.infra.entities.SubscriptionFromRoom
+
+class SubscriptionRepositoryImplementation(private val subscriptionDAO: SubscriptionDAO): SubscriptionsRepository {
+    override suspend fun getAllSubscriptions(): List<Subscription> {
+        return subscriptionDAO.getAllSubscriptions().map {
+            Subscription(it.subscriptionId)
+        }
+    }
+
+    override suspend fun createSubscription(subscription: Subscription) {
+        subscriptionDAO.insertSubscription(SubscriptionFromRoom(subscription.subscriptionId))
+    }
 }
--- app/src/main/java/com/example/onemessagechat/domain/repositories/SubscriptionsRepository.kt
+++ app/src/main/java/com/example/onemessagechat/domain/repositories/SubscriptionsRepository.kt
@@ -4,4 +4,5 @@ import com.example.onemessagechat.domain.entities.subscribe.Subscription
 
 interface SubscriptionsRepository {
     suspend fun getAllSubscriptions(): List<Subscription>
+    suspend fun createSubscription(subscription: Subscription): Unit
 }
